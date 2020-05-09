42705b0ff8ee61a964cf99d864c07836a53105b5
Quackenstien <donaldhawkins422@gmail.com>
2020-05-09 12:20:38 -0700

    display questions and answer choices only when start quiz btn is pushed

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
 var timerEl = document.getElementById("countdown");
 var highBtnEl = document.getElementById("highscores");
 var startQuiz = document.getElementById("startquiz");
+var questionOne = document.querySelector("#q1");
+var answerOne = document.querySelector("#a1");
+var answerTwo = document.querySelector("#a2");
+var answerThree = document.querySelector("#a3");
+var answerFour = document.querySelector("#a4");
+
+var timeLeft = 5;
+// document.getElementById("a1");
+
+questions = [{ title: "who let the dogs out", answer: "" }];
 
 function startCountdown() {
-  var timeLeft = 70;
-
   var timeInterval = setInterval(function () {
     timeLeft--;
     timerEl.textContent = timeLeft;
+
+    questionOne.textContent = "Question#2";
+    answerOne.textContent = "phoebe";
+    answerTwo.textContent = "cow";
+    answerThree.textContent = "cow";
+    answerFour.textContent = "cow";
 
     if (timeLeft === 0) {
       clearInterval(timeInterval);
@@ -16,4 +30,10 @@ function startCountdown() {
   return;
 }
 
+// function displayQuestions() {
+//   questionOne.textContent = " Who is...?";
+//   answerOne.textContent = "Bob";
+// }
+
 startQuiz.addEventListener("click", startCountdown);
+// startQuiz.addEventListener("click", displayQuestions);
