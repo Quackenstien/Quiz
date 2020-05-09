b34bccf29a303f9571a30bd8e47ac0582094e1e5
Quackenstien <donaldhawkins422@gmail.com>
2020-05-09 13:06:57 -0700

    created ol items using js and populated answers using objects

--- script.js
+++ script.js
@@ -2,14 +2,23 @@ var timerEl = document.getElementById("countdown");
 var highBtnEl = document.getElementById("highscores");
 var startQuiz = document.getElementById("startquiz");
 var questionOne = document.querySelector("#q1");
-var answerOne = document.querySelector("#a1");
-var answerTwo = document.querySelector("#a2");
-var answerThree = document.querySelector("#a3");
-var answerFour = document.querySelector("#a4");
+// var answerOne = document.querySelector("#a1");
+// var answerTwo = document.querySelector("#a2");
+// var answerThree = document.querySelector("#a3");
+// var answerFour = document.querySelector("#a4");
+var tag = document.createElement("li");
+var secondTag = document.createElement("li");
+var thirdTag = document.createElement("li");
+var fourthTag = document.createElement("li");
 
 var timeLeft = 5;
 
-var allAnswers = { answer1: "who let the dogs out", answer2: "bob" };
+var answerObj = {
+  ans1: "who let the dogs out",
+  ans2: "bob",
+  ans3: "get",
+  ans4: "tree",
+};
 
 function startCountdown() {
   var timeInterval = setInterval(function () {
@@ -17,10 +26,19 @@ function startCountdown() {
     timerEl.textContent = timeLeft;
 
     questionOne.textContent = "Not me...";
-    answerOne.textContent = allAnswers.answer1;
-    answerTwo.textContent = allAnswers.answer2;
-    answerThree.textContent = "cow";
-    answerFour.textContent = "cow";
+    // answerOne.textContent = answerObj.ans1;
+    // answerTwo.textContent = answerObj.ans2;
+    // answerThree.textContent = answerObj.ans3;
+    // answerFour.textContent = answerObj.ans4;
+    document.body.children[3].appendChild(tag);
+    document.body.children[3].appendChild(secondTag);
+    document.body.children[3].appendChild(thirdTag);
+    document.body.children[3].appendChild(fourthTag);
+
+    tag.textContent = answerObj.ans1;
+    secondTag.textContent = answerObj.ans2;
+    thirdTag.textContent = answerObj.ans3;
+    fourthTag.textContent = answerObj.ans4;
 
     if (timeLeft === 0) {
       clearInterval(timeInterval);
