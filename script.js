2e1c932a7342bd745e45739e515c161b50c994b9
Quackenstien <donaldhawkins422@gmail.com>
2020-05-09 14:49:12 -0700

    new function trigger

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
 var timerEl = document.getElementById("countdown");
 var highBtnEl = document.getElementById("highscores");
 var startQuiz = document.getElementById("startquiz");
-var questionOne = document.querySelector("#q1");
-// var answerOne = document.querySelector("#a1");
-// var answerTwo = document.querySelector("#a2");
-// var answerThree = document.querySelector("#a3");
-// var answerFour = document.querySelector("#a4");
+var question = document.querySelector("#q1");
 var tag = document.createElement("li");
 var secondTag = document.createElement("li");
 var thirdTag = document.createElement("li");
@@ -14,26 +10,63 @@ var tagBtn = document.createElement("button");
 var tagBtn2 = document.createElement("button");
 var tagBtn3 = document.createElement("button");
 var tagBtn4 = document.createElement("button");
+var listEl = document.querySelector("#listEl");
 
-var timeLeft = 5;
+var timeLeft = 25;
 
 var answerObj = {
   ans1: "who let the dogs out",
   ans2: "bob",
   ans3: "get",
   ans4: "tree",
+  ans5: "hill",
+  ans6: "cat",
+  ans7: "dog",
+  ans8: "chewy",
 };
 
 function startCountdown() {
   var timeInterval = setInterval(function () {
     timeLeft--;
     timerEl.textContent = timeLeft;
+    questOne();
 
-    questionOne.textContent = "Not me...";
-    // answerOne.textContent = answerObj.ans1;
-    // answerTwo.textContent = answerObj.ans2;
-    // answerThree.textContent = answerObj.ans3;
-    // answerFour.textContent = answerObj.ans4;
+    function questOne() {
+      question.textContent = "Not me...";
+
+      document.body.children[3].appendChild(tag);
+      document.body.children[3].appendChild(secondTag);
+      document.body.children[3].appendChild(thirdTag);
+      document.body.children[3].appendChild(fourthTag);
+      document.body.children[3].children[0].appendChild(tagBtn);
+      document.body.children[3].children[1].appendChild(tagBtn2);
+      document.body.children[3].children[2].appendChild(tagBtn3);
+      document.body.children[3].children[3].appendChild(tagBtn4);
+
+      tagBtn.textContent = answerObj.ans1;
+      tagBtn2.textContent = answerObj.ans2;
+      tagBtn3.textContent = answerObj.ans3;
+      tagBtn4.textContent = answerObj.ans4;
+
+      listEl.addEventListener("click", function (event) {
+        event.preventDefault();
+        if ((event.target = tagBtn.value)) {
+          questTwo();
+        } else {
+          timeLeft-- * 10;
+          questTwo();
+        }
+      });
+    }
+    if (timeLeft === 0) {
+      clearInterval(timeInterval);
+    }
+  }, 1000);
+  return;
+
+  function questTwo() {
+    question.textContent = "Yes me...";
+
     document.body.children[3].appendChild(tag);
     document.body.children[3].appendChild(secondTag);
     document.body.children[3].appendChild(thirdTag);
@@ -43,18 +76,21 @@ function startCountdown() {
     document.body.children[3].children[2].appendChild(tagBtn3);
     document.body.children[3].children[3].appendChild(tagBtn4);
 
-    console.log(event);
+    tagBtn.textContent = answerObj.ans5;
+    tagBtn2.textContent = answerObj.ans6;
+    tagBtn3.textContent = answerObj.ans7;
+    tagBtn4.textContent = answerObj.ans8;
 
-    tagBtn.textContent = answerObj.ans1;
-    tagBtn2.textContent = answerObj.ans2;
-    tagBtn3.textContent = answerObj.ans3;
-    tagBtn4.textContent = answerObj.ans4;
-
-    if (timeLeft === 0) {
-      clearInterval(timeInterval);
-    }
-  }, 1000);
-  return;
+    listEl.addEventListener("click", function (event) {
+      event.preventDefault();
+      if ((event.target = tagBtn4)) {
+        console.log("hi");
+        // questThree();
+      } else {
+        timeLeft-- * 10;
+        // questThree();
+      }
+    });
+  }
 }
-
 startQuiz.addEventListener("click", startCountdown);
