48317e821d0681221d61f16b81223ec3cacfbc07
Quackenstien <donaldhawkins422@gmail.com>
2020-05-11 18:42:28 -0700

    added new name for leader board js and js to access the scores

---
+++ leaderboardscript.js
@@ -0,0 +1,19 @@
+var bodyEl = document.querySelector("body");
+var ulEl = document.querySelector("ul");
+
+var score = "";
+var user = "";
+
+// Pull recent score stats from local storage
+function init() {
+  score = localStorage.getItem("score");
+  user = localStorage.getItem("user");
+
+  ulEl.append(user);
+  ulEl.append(score);
+
+  console.log(score);
+  console.log(user);
+}
+
+init();
